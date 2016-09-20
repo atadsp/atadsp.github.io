5313526cddd1718e0b8efb6e6230dada0729b378
Chris Beckman <atadsp@gmail.com>
2016-09-20 14:08:15 -0400

    making most recent updates live

--- scripts/quick_functions.js
+++ scripts/quick_functions.js
@@ -1,7 +1,3 @@
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
-
-Array.prototype.randomElement = function () {
-    return this[Math.floor(Math.random() * this.length)];
-};
